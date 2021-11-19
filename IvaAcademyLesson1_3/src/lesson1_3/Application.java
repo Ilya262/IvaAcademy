8ae3ba5c16e0507ff298f2a38370a758d1634a65
ilchik262 <ilchik262@gmail.com>
2021-11-19 17:28:30 +0200

    Lesson 4

---
+++ IvaAcademyLesson1_3/src/lesson1_3/Application.java
@@ -0,0 +1,52 @@
+package lesson1_3;
+
+import java.util.Random;
+
+public class Application {
+
+//	Создайте массив из 15 случайных целых чисел из отрезка [0;9]. 
+//	Выведите массив на экран. Подсчитайте сколько в массиве чётных элементов и 
+//	выведете это количество на экран на отдельной строке.
+//	
+
+	public static void main(String[] args) {
+
+//		int count = 0;
+//
+//		int[] array = new int[15];
+//
+//		for (int i = 0; i < array.length; i++) {
+//
+//			Random random = new Random();
+////			array[i]=i;
+//
+//			array[i] = random.nextInt(10);
+////			double a = Math.random()*9;
+////			 array[i]=a;
+//			System.out.print(array[i] + " ");
+//			
+//			if (array[i] % 2 == 0) {
+//
+//				count++;
+//
+//			}
+//			if (array[i] == 0) {
+//
+//				count--;
+//			}
+//
+//		}
+//		System.out.println();
+//		
+//	
+//		int a=100;
+//		int b=0;
+//		while(a>b) {
+//			a--;
+//			b++;
+//		}
+//		System.out.println(b);
+//	    
+
+	}
+}
