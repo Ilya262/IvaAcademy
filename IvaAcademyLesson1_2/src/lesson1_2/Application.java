4180eb81fe3568e5e95ad5b48b955c3a78b46ca4
ilchik262 <ilchik262@gmail.com>
2021-11-19 17:25:04 +0200

    lesson 2

---
+++ IvaAcademyLesson1_2/src/lesson1_2/Application.java
@@ -0,0 +1,144 @@
+package lesson1_2;
+
+import java.util.Random;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+//		int a=10;//задали змінну
+//		while(a>0) { //умова циклу
+//		
+//			System.out.println(" count " + a);//вивід 10-9-8-7-6-5-4-3-2-1
+//			a--;//інкрементація
+//		}
+	
+//		String quote="Я";
+//		int a=100;
+//		while(a>0) {
+//			
+//			System.out.println(a+" - "+quote);
+//		a--;
+//		}
+//		System.out.println("-----------------------");
+//		int a=10;
+//		do {
+//			a--;
+//			System.out.println("i love \"do while\" ");
+//		}
+//		while(a<0) ;
+//			
+//		
+
+//		System.out.println("-----------------------");
+
+//		int a=9;//після циклу значення 8
+//		int b=4;
+//		int c=9;
+//		
+//		
+//		while(a==b||a==c) {
+//			
+//			System.out.println("aaa");
+//			a--;
+//		}
+//		
+//		System.out.println("-----------------------");
+//		int [] array=new int [10];
+//		
+//		for (int i=0;i<array.length;i++) {
+//			
+//			array[i]=i;
+//			System.out.println(array[i]);
+//		}
+//		
+//		System.out.println("-----------------------");
+//		
+//		int [] array2=new int [10];
+//		
+//		for(int i=array2.length-1;i>=0;i--) {
+//			
+//			array2[i]=i;
+//			
+//			System.out.println(array2[i]);
+//		}
+//	
+//		System.out.println("-----------------------");
+//		int [] array3=new int[22];
+//		
+//		for(int i=1;i<array3.length;i++) {
+//			array3[i]=i;
+//			if(array3[i]%2==0) {
+//				System.out.print(array3[i]+" ");
+//				
+//			}
+//			
+//		}
+//		System.out.println();
+//		for(int i=1;i<array3.length;i++) {
+//			array3[i]=i;
+//			if(array3[i]%2==0) {
+//				System.out.println(array3[i]);
+//				
+//			}
+//			
+//		}
+//		
+//		System.out.println("-----------------------");
+//		  int a=0;
+//	        for(int i=2;i<=20;i++){
+//	            if(i%2==0)a++;
+//	        }
+//	        //Создадим массив, и используя цикл, заполним его ячейки. Сразу выведем на экран значения элементов массива в строку
+//	        int[] Mas = new int[a];
+//	        for(int i=2,b=0;i<=20;i++){
+//	                if(i%2==0){
+//	                    Mas[b]=i;
+//	                    System.out.print(Mas[b]+" ");
+//	                    b++;
+//	                }
+//	        }
+//	        //Выведем на экран каждый элемент массива на отдельной строке
+//	        System.out.println(" ");
+//	        for(int i=0;i<Mas.length;i++){
+//	            System.out.println(Mas[i]);
+//	        }
+	    
+	////////////////////////////////////
+		
+		
+		
+//		Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, 
+//		а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3 1).
+		
+		
+		
+//		int [] array4=new int[100];
+//		
+//		for(int i=0;i<array4.length;i++) {
+//			array4[i]=i;
+////			array4=2,3,5,7;
+//			
+//			if(array4[i]%2!=0) {
+//				System.out.println(array4[i]);
+//			}
+//			
+//		}
+//		System.out.println("-----------------------");
+		
+//		int [] array5=new int [100];
+//		
+//		for(int i=array5.length-1;i>=0;i--) {
+//			
+//			array5[i]=i;
+//			
+//			System.out.println(array5[i]);
+//		}
+		
+		
+	}	
+		
+		
+	
+
+}
