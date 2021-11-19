4c6c2d6941c361352d87ef6cc02500df540e9cc9
ilchik262 <ilchik262@gmail.com>
2021-11-19 17:08:54 +0200

    Lesson 1 Variable and basic function

---
+++ IvaAcademyLesson1_1/IvaAcademyLesson1_1/src/lesson1_1/Aplication.java
@@ -0,0 +1,66 @@
+package lesson1_1;
+
+public class Aplication {
+
+	public static void main(String[] args) {
+
+////
+////		int boxApple = 30;
+////
+//	double boxAppleDouble = 29.5;
+////
+////		double sum = boxApple + boxAppleDouble;
+////		
+////		System.out.println("summa apple " + sum+ " znopw summa " + sum);
+////		System.out.print("lol ");
+////		System.out.println("summa apple " + sum+ " znopw summa " + sum);
+//		
+//		String studentName="Oleg";
+//
+//		String familiya="Ivanovych";
+//	
+//		int age=21;
+//		
+
+//		System.out.println(studentName+" "+familiya+" "+age+ " has "+ boxAppleDouble);
+//		int a=2;
+//		int b=2;
+//if(a!=b) {
+//	
+//	System.out.println("ne rivne");
+//}
+//else {
+//	System.out.println("rivne");
+//}
+
+//		int a = 10;
+//		int b = 25;
+//		int c = 15;
+//		if (a > b) {
+//			System.out.println("bilche");
+//		}
+//
+//		else if (a > c) {
+//
+//			System.out.println("a>c");
+//		}
+//
+//		else {
+//			System.out.println("a naymencha");
+//		}
+		//////////////////////////////////////////////////
+//		 if(a>b) {
+//			 System.out.println("bilche");
+//		 }
+//		 else  {
+//			 
+//			 System.out.println("min number");
+//		 }
+
+		
+
+	
+		
+	}
+
+}
